2c7c4f89bec5affded50e2928cd47e50a3bedc14
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-08-26 23:14:37 -0400

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,28 +1,5 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    let sicon = message.guild.iconURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#d604cf")
-    .setThumbnail(sicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt)
-    .addField("Author", "FluffyThePanda");
-
-    message.channel.send(botembed);
-
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
-    .setColor("#d604cf")
-    .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Server Owner", message.guild.owner)
-    .addField("Created On", message.guild.createdAt)
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Total Members", message.guild.memberCount);
-
-    message.channel.send(serverembed);
-
     let joinembed = new Discord.RichEmbed()
     .setTitle("Join Information")
     .setColor("#d604cf")

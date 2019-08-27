c53bce376b7a414da1b129662e0c2879b642df1b
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-08-26 23:43:41 -0400

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -3,15 +3,17 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
+    .setTitle("Join Information")
+    .setColor("#d604cf")
+    .setThumbnail(sicon)
+    .addField("Server Owner", message.guild.owner)
+    .addField("Name", message.guild.name)
+    .addField("ID", message.guild.Id)
+    .addField("Members", message.guild.memberCount);
 
     message.channel.send(botembed);
 }
 
 module.exports.help = {
-  name:"botinfo"
+  name:"owner"
 }
